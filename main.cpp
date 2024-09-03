5431c9305e6da28bcdaa9cb9152fe339e11c7041
jhopkins0 <156948029+jhopkins0@users.noreply.github.com>
2024-09-03 11:37:50 -0700

    edit

--- main.cpp
+++ main.cpp
@@ -3,13 +3,18 @@ using namespace std;
 
 int main()
 {
-	int n1, n2;
+	int n1, n2, n3;
 	int N;
 
 	cin >> n1 >> n2;
 	cin >> N;
 
-	/***************************************************
-	 * Code your program here
-	 **************************************************	*/
+	cout << n1 << "\t" << n2;
+	
+	while (i < N-2){
+		n3 = n1 + n2;
+		cout << n3 << "\t";
+		n1 = n2;
+		n2 = n3;
+	}
 }
